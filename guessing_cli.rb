5f75dc720e3c6686915a6e801d2d64c32c34042f
Sean Lucius <seanlucius22@icloud.com>
2019-02-12 23:46:41 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -2,6 +2,7 @@ def run_guessing_game
   loop do 
     puts "Guess a number between 1 and 6."
     user_guess = gets.strip
-    computer_guess = Math.random()
+    computer_guess = Math.random(1..6)
+    
   end 
 end 
