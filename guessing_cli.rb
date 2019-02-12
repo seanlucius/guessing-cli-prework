370063fc9dd953328a7ad5aa8418651659f360a0
Sean Lucius <seanlucius22@icloud.com>
2019-02-12 23:49:19 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,6 +3,10 @@ def run_guessing_game
     puts "Guess a number between 1 and 6."
     user_guess = gets.strip
     computer_guess = rand(1..6)
-    
+    if user_guess.to_i == computer_guess
+      puts "You guessed the correct number!"
+    else 
+      puts "The computer guessed #{computer_guess}."
+    end 
   end 
 end 
