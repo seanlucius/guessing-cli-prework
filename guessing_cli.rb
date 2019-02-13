d3ab7b3d99fdc7fae142fc380d2b1a68b2802e4f
Sean Lucius <seanlucius22@icloud.com>
2019-02-13 00:25:37 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,10 +7,8 @@ def run_guessing_game
       puts "You guessed the correct number!"
     elsif user_guess.to_i != computer_guess 
       puts "The computer guessed #{computer_guess}."
-    elsif user_guess == "exit"
-      puts "Goodbye!"
     end
-    break
+    break if user_guess == "exit"
   end 
   puts "Goodbye!"
 end 
